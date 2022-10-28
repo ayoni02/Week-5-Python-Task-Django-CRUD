6f37799e72467fe92114a09db4d8b9c2463b10cc
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2022-10-28 20:25:47 +0100

    made changes

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Artist
+from .models import Artist, Lyric, Song
 
 admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(Lyric)
--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -4,16 +4,19 @@ from django.db import models
 # Create your models here.
 
 class Artist(models.Model):
-    First_name = models.CharField(max_length=200)
-    Last_name = models.CharField(max_length=200)
-    Age = models.CharField(max_length=200)
+    First_name = models.CharField(max_length=100)
+    Last_name = models.CharField(max_length=100)
+    Age = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.First_name + " " + self.Last_name
     
 class Song(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     date_released = models.DateTimeField('date published')
     likes = models.IntegerField(default=0)
     artiste_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
     
 class Lyric(models.Model):
-    content = models.CharField(max_length=200)
+    content = models.TextField()
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
